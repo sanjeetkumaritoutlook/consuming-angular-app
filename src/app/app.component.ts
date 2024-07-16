d4a21fee15b1c00824c4a1af13ecc45c160c0474
unknown <sanjeetkumarit@outlook.com>
2024-07-16 11:27:11 +0530

    angular application consuming fluid-library package from npm public registry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FluidServiceService } from 'fluid-library';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'consuming-angular-app';
+  message: string | undefined;
+  constructor(private myService: FluidServiceService) { }
+
+  ngOnInit() {
+    this.message = this.myService.getMessage();
+   
+  }
 }
